3f38830ab43b6679636e8bc6d90232d11e60f63f
almyk <hansentommy92@gmail.com>
2019-06-04 15:05:05 +0900

    Create skeleton file for the blockchain node

---
+++ Blockchain_demo.py
@@ -0,0 +1,12 @@
+import Node
+
+class BlockchainNode(Node.Node):
+
+    def __init__(self, host, port, callback=None):
+        super(BlockchainNode, self).__init__(host, port, callback)
+
+        self.rsa_key = None
+
+    def eventNodeMessage(self, node, data):
+        # TODO: handle different kinds of communications between nodes here
+        pass
